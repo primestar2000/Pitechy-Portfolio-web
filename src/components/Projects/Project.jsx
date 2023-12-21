7e666e27de67751f4f151e79da55fb5653bb0fa2
stephen simon <primestar2000@gmail.com>
2023-12-21 05:04:11 -0800

    gh pages deploy commit3

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -7,7 +7,7 @@ const Project = () => (
             <div className="lg:w-1/2 p-4  lg:p-10 text-slate-500 dark:text-white">
                 <h2 className="font-bold text-2xl text-center lg:text-left my-2">HealthPAl AI</h2>
                 {/* <p className="font-semibold text-xl text-center lg:text-left"><i>{`Website`}</i></p> */}
-                <p className="text-sm text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora velit neque saepe, at quo nesciunt temporibus enim qui inventore cupiditate? </p>
+                <p className="text-sm ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora velit neque saepe, at quo nesciunt temporibus enim qui inventore cupiditate? </p>
                 <div className="flex gap-4">
                     <Tag content={"React js"} />
                     <Tag content={"OpenAi API"} />
@@ -16,7 +16,7 @@ const Project = () => (
             </div>
             <div className=" shadow-md lg:w-[500px] w-full h-[350px] rounded-xl flex justify-center items-center bg-black" style={{backgroundImage: `url(${projectBgImage})`}}>
                 <div className="w-[90%] h-[90%] flex justify-center items-center ">
-                    <img src={projectImage} alt="image of health pal ai" className="rounded-xl object-cover h-full w-full" />
+                    <img src={projectImage} alt="image of health pal ai" className="rounded-xl object-cover  w-full" />
                 </div>
             </div>
         </div>
